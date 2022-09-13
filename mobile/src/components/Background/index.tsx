47fd5bd39f7341e1e5de1c4add302af7cef75795
Guilherme <guiggff@gmail.com>
2022-09-13 10:38:05 -0300

    segundo dia

---
+++ mobile/src/components/Background/index.tsx
@@ -0,0 +1,21 @@
+import { ImageBackground } from "react-native";
+
+import backgroundImage from "../../assets/background-galaxy.png";
+
+import { styles } from "./styles";
+
+interface BackgroundProps {
+  children: React.ReactNode;
+}
+
+export const Background = ({ children }: BackgroundProps) => {
+  return (
+    <ImageBackground
+      style={styles.container}
+      source={backgroundImage}
+      defaultSource={backgroundImage}
+    >
+      {children}
+    </ImageBackground>
+  );
+};
---
+++ mobile/src/screens/Home/index.tsx
@@ -0,0 +1,33 @@
+import { Image, View, FlatList } from "react-native";
+
+import logoImg from "../../assets/logo-nlw-esports.png";
+
+import { GAMES } from "../../utils/games";
+
+import { Heading } from "../../components/Heading";
+import { GameCard } from "../../components/GameCard";
+
+import { styles } from "./styles";
+
+export const Home = () => {
+  return (
+    <View style={styles.container}>
+      <Image source={logoImg} style={styles.logo} />
+
+      <Heading
+        title="Encontre seu duo!"
+        subtitle="Selecione o game que deseja jogar..."
+      />
+
+      <FlatList
+        data={GAMES}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <GameCard data={item} />}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={styles.contentList}
+      />
+    </View>
+  );
+};
